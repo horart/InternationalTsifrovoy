f7d79365af2ee19d3a4777f8950f7ab49b058c3c
Artyom <artem.khorev7@ya.ru>
2024-11-08 17:44:45 +0300

    initial structure and react basis

---
+++ server/server.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+ctr = 0
+# Serve React App
+@app.route('/api')
+def api():
+    global ctr
+    ctr += 1
+    print(ctr)
+    return str(ctr)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5000, threaded=True)
